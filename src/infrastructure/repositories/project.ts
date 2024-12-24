59a1f2f35baef8f1dbd0582fb468ae1be4ab650d
Indra Zulfi <me@indrazm.com>
2024-12-24 19:03:34 +0700

    adding project repositories

--- src/infrastructure/repositories/project.ts
+++ src/infrastructure/repositories/project.ts
@@ -1,7 +1,46 @@
 import { prisma } from "../utils/prisma";
+import type { Project } from "@prisma/client";
 
 export class ProjectRepositories {
-	public async getProjects(userId: string) {
-		return await prisma.project.findMany({});
+	public async getAll(userId: string) {
+		return await prisma.project.findMany({
+			where: {
+				userId,
+			},
+		});
+	}
+
+	public async get(projectIdOrSlug: string) {
+		return await prisma.project.findFirst({
+			where: {
+				OR: [
+					{
+						id: projectIdOrSlug,
+					},
+					{
+						slug: projectIdOrSlug,
+					},
+				],
+			},
+		});
+	}
+
+	public async create(data: Omit<Project, "id">) {
+		return await prisma.project.create({
+			data,
+		});
+	}
+
+	public async addContent(projectId: string, content: string) {
+		return await prisma.content.create({
+			data: {
+				content,
+				project: {
+					connect: {
+						id: projectId,
+					},
+				},
+			},
+		});
 	}
 }
