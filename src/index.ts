a68a2903431bae89782ba1292850bac754e2ee7c
Indra Zulfi <me@indrazm.com>
2024-12-25 15:38:06 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ import { Elysia } from "elysia";
 import { authRouter } from "./presentation/routes/auth";
 import { projectRouter } from "./presentation/routes/project";
 import cors from "@elysiajs/cors";
-import swagger from "@elysiajs/swagger";
 import { serverAdapter } from "./infrastructure/utils/bullmq";
 import staticPlugin from "@elysiajs/static";
 
 export const _ = new Elysia()
-  // plugins
-  .use(staticPlugin())
-  .use(cors())
-  .use(serverAdapter.registerPlugin())
+	// plugins
+	.use(staticPlugin())
+	.use(cors())
+	.use(serverAdapter.registerPlugin())
 
-  // routes
-  .use(authRouter)
-  .use(projectRouter)
+	// routes
+	.use(authRouter)
+	.use(projectRouter)
 
-  // port listening
-  .listen(8000, () => {
-    console.log("RAG Backend ran at port 8000");
-  });
+	// port listening
+	.listen(8000, () => {
+		console.log("RAG Backend ran at port 8000");
+	});
